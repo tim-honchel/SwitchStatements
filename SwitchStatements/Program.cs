02028489df11f69e0e0b84c462525813c4f053df
Tim Honchel <tim.honchel@gmail.com>
2021-12-07 14:33:59 -0500

    completed exercise

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            Console.WriteLine("");
+            string favoriteSubject = Console.ReadLine().ToLower();
+            Console.WriteLine("");
+
+            switch (favoriteSubject)
+            {
+                case "math":
+                    Console.WriteLine("Math? I know how to plus and take away.");
+                    break;
+                case "english":
+                    Console.WriteLine("English? Hey I speak that language!");
+                    break;
+                case "history":
+                    Console.WriteLine("History? That happened a long time ago.");
+                    break;
+                case "science":
+                    Console.WriteLine("Science? We don't believe in that here.");
+                    break;
+                case "art":
+                    Console.WriteLine("Art? How's that working out for you?");
+                    break;
+                default:
+                    Console.WriteLine(favoriteSubject.ToUpperInvariant() + "? I've heard about that but I didn't think it was real.");
+                    break;
+            }
+        }
+    }
+}
